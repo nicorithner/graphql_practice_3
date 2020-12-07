f65dd4adfb7d0eb226f2a79c900ecbd6d7e90a5b
Nico Rithner <nicorithner@gmail.com>
2020-12-07 15:47:07 -0700

    Install graphql, express-graphql, express and axios

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use('/graphl', graphqlHTTP({
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port $${PORT}`););
+app.listen(PORT, () => console.log(`Server started on port $${PORT}`));
