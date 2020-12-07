5f2aefd8350b380eef2dd083fd06fc1ba4bf64f0
Nico Rithner <nicorithner@gmail.com>
2020-12-07 15:13:47 -0700

    Create sever.js file and add initial configuration
    Need a schema file to complete

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const grapqlHTTP = require('express-graphql')
+const schema = require('./schema')
+
+const app = express();
+
+app.use('/graphl', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port $${PORT}`););
